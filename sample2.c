4fd823ca7968e43acf8604df68d213a3b1228417
Ahmed Khalil <>
2020-05-25 07:57:43 +0000

    source with include

---
+++ sample2.c
@@ -0,0 +1,14 @@
+// Sample file to tokenise
+
+#include <stdatomic.h>
+
+double i = 4;
+extern float x;
+typedef long long longlongtype;
+
+int main()
+{
+	typedef unsigned int uinttype;
+	printf("msg exp\n");
+	return 0;
+}
